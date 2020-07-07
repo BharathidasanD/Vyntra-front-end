1ffe0bb3e4d445d2edfea2fd9f91c50a335e598b
bharathidasan.d1999@gmail.com <bharathidasan.d1999@gmail.com>
2020-07-07 19:36:36 +0530

    images changed

--- src/app/imageslider/imageslider.component.ts
+++ src/app/imageslider/imageslider.component.ts
@@ -8,20 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ImagesliderComponent implements OnInit {
 
   imageObject = [{
-    image: 'assets/images/gown.jpg',
-    thumbImage: 'assets/images/gown.jpg',
-    title: 'Hummingbirds are amazing creatures'
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbpGyjUyWCjhuz4UUUrmIjaog-ylPC3IKrbA&usqp=CAU',
+    thumbImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbpGyjUyWCjhuz4UUUrmIjaog-ylPC3IKrbA&usqp=CAU'
+    
 }, {
-    image: 'assets/images/formal.jpg',
-    thumbImage: 'assets/images/formal.jpg'
+    image: 'https://c4.wallpaperflare.com/wallpaper/675/749/84/cinderella-dress-blue-art-wallpaper-preview.jpg',
+    thumbImage: 'https://c4.wallpaperflare.com/wallpaper/675/749/84/cinderella-dress-blue-art-wallpaper-preview.jpg'
 }, {
-    image: 'assets/images/coat.jpg',
-    thumbImage: 'assets/images/coat.jpg',
-    title: 'Example with title.'
+    image: 'https://www.dhresource.com/0x0s/f2-albu-g4-M01-99-18-rBVaEFgYTsaAJFkSAAIX4EkPEMk283.jpg/stunning-pink-flower-girl-dresses-satin-kids.jpg',
+    thumbImage: 'https://www.dhresource.com/0x0s/f2-albu-g4-M01-99-18-rBVaEFgYTsaAJFkSAAIX4EkPEMk283.jpg/stunning-pink-flower-girl-dresses-satin-kids.jpg'
 }, {
-    image: 'assets/images/pant.jpg',
-    thumbImage: 'assets/images/pant.jpg',
-    title: 'Example two with title.'
+    image: 'https://www.itl.cat/pngfile/big/194-1940788_punjabi-suits-designs-for-girls-punjabi-dress-for.jpg',
+    thumbImage: 'https://www.itl.cat/pngfile/big/194-1940788_punjabi-suits-designs-for-girls-punjabi-dress-for.jpg'
+   
 }];
   constructor() { }
 
