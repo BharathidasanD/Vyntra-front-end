aa037f636d3a06eb341ce04f65998a58606e1e64
BharathidasanDurai <46603961+BharathidasanD@users.noreply.github.com>
2020-07-08 12:48:02 +0530

    Updated Backend
    please use this

--- backend/VyntraDemo/src/main/java/com/vyntrademo/model/User.java
+++ backend/VyntraDemo/src/main/java/com/vyntrademo/model/User.java
@@ -15,21 +15,30 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	protected Long user_id;
+	protected Long userId;
 
 	@NotNull
-	protected String user_password;
+	protected String userPassword;
 	
+	@NotNull
+	protected String role;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 	public Long getUser_id() {
-		return user_id;
+		return userId;
 	}
 	public void setUser_id(Long user_id) {
-		this.user_id = user_id;
+		this.userId = user_id;
 	}
 	public String getUser_password() {
-		return user_password;
+		return userPassword;
 	}
 	public void setUser_password(String user_password) {
-		this.user_password = user_password;
+		this.userPassword = user_password;
 	} 
 }
